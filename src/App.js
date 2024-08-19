cd567907ff47d2d9d67dbf38ea3f74bedee63457
varushchi <vadim.shchigolev#mail.ru>
2024-08-19 16:51:01 +0300

    player movement

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css'
-import Player from './Player';
+//import Player from './Player';
+import MovePlayer from './MovePlayer';
 
 function App() {
+
+  const top1 = MovePlayer(0)
+  const top2 = MovePlayer(500)
   return (
     <div className="App">
       <div className="play-border">
-        <Player
-          width = '100'
-          height = 'auto'
-          viewBox = '0 0 24 1000'
-          color = 'red'
-          x = '10'
-          y = '100'
-          radius = '20'
-        />
-
-        <Player
-          width = '100'
-          height = 'auto'
-          viewBox = '0 0 24 1000'
-          color = 'green'
-          x = '10'
-          y = '100'
-          radius = '20'
-        />
+        <div className='player-1' style={{top: top1}}></div>
+        <div className='player-2' style={{top: top2}}></div>
       </div>
     </div>
   );
--- src/Player.js
+++ src/Player.js
@@ -2,7 +2,7 @@
 
 function Player(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} viewBox={props.viewBox} fill={props.color} stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><circle cx={props.x} cy={props.y} r={props.radius}></circle></svg>
+    <svg className = 'player' xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} viewBox={props.viewBox} fill={props.color} stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><circle cx={props.x} cy={props.y} r={props.radius}></circle></svg>
   );
 }
 
